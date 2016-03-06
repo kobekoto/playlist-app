416b6af063b17c34bcb7163f58dcdc4ca3257a1f
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2016-03-06 05:34:07 -0500

    adding more gulp tasks (browserSync, watch, serve-dev, runSequence)

--- gulp.config.js
+++ gulp.config.js
@@ -1,4 +1,4 @@
-// File all
+// This file helps keep our gulpfile a little bit cleaner!
 
 module.exports = function() {
     var src = 'src/';
@@ -9,16 +9,35 @@ module.exports = function() {
         appjs: [
             src + 'app/**/*.js'
         ],
-        
+        apphtml: src + 'app/**/*.html',        
         // all js to check
-        alljs: [
-            './src/**/*.js',
-            './*.js'
-        ],
+        alljs: './*.js',
 
         css: publicFolder + 'styles.css',
+        index: src + 'index.html',
         publicStyles: publicFolder + 'styles/',
-        sass: './src/styles/**/*.scss'
-        
+        sass: './src/styles/**/*.scss',
+
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../'
+        },
+
+        defaultPort: 8000,
+        nodeServer: 'server.js'
+
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
+    };
+
+
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,39 @@ var port = process.env.PORT || config.defaultPort;
 //check dev dependencies in package.json to remember which plugins are being used
 var $ = require('gulp-load-plugins')({lazy: true});
 
+gulp.task('copy-bower-components-dev', function() {
+	gulp.src(['./bower_components/**/*.min.*', './lib/**/*.min.*'])
+		.pipe(gulp.dest('./public/vendor'));
+});
+
+gulp.task('copy-app-dev', function() {
+	gulp.src(config.appjs)
+		.pipe(gulp.dest('./public/app'));
+});
+
+gulp.task('copy-templates-dev', function() {
+	gulp.src(config.apphtml)
+		.pipe(gulp.dest('./public/app'));
+});
+
+gulp.task('copy-index-dev', function() {
+	gulp.src(config.index)
+		.pipe(gulp.dest('./public'));
+});
+
 gulp.task('check-js', function() {
 	log('Analyzing HS files with JSHint and JSCS');
 
 	return gulp
         .src(config.alljs)
  // run gulp check-js --verbose to print out all the files that gulp is running
-    		.pipe($.if(args.verbose, $.print()))
-    		.pipe($.jscs())
-	        .pipe($.jshint())
-	        .pipe($.jshint.reporter('jshint-stylish', {
-	            verbose: true
-	        }))
-        	.pipe($.jshint.reporter('fail'));
+		.pipe($.if(args.verbose, $.print()))
+		.pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+   		.pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('styles', function() {
@@ -33,6 +53,95 @@ gulp.task('styles', function() {
 		.pipe($.autoprefixer({ browsers: ['last 2 version', '> 5%']}))
 		.pipe(gulp.dest(config.publicStyles));
 });
+
+gulp.task('watch', function() {
+	gulp.watch([config.appjs], ['check-js', 'dev']).on('change', browserSync.reload);
+	gulp.watch([config.apphtml], ['dev']).on('change', browserSync.reload);
+	gulp.watch([config.index], ['dev']).on('change', browserSync.reload);
+	gulp.watch([config.nodeServer], ['check-js', 'dev']).on('change', browserSync.reload);
+	gulp.watch([config.sass], ['styles', 'dev']).on('change', browserSync.reload);
+});
+
+
+
+gulp.task('serve-dev', ['watch'], function() {
+
+	var nodeOptions = {
+		script: config.nodeServer,
+		delayTime: 1,
+		env: {
+			'PORT': port
+		},
+		watch: [config.nodeServer]
+	};
+
+	return $.nodemon (nodeOptions)
+		.on('restart', ['check-js'], function(ev) {
+			log('*** nodemon restarted');
+			log('files changed on restart:\n' + ev);
+			setTimeout(function() {
+				browserSync.notify('reloading now ....');
+				browserSync.reload({stream: false});
+			}, 1000);
+		})
+		.on('start', function() {
+			log('*** nodemon started');
+			startBrowserSync();
+		})
+		.on('crash', function() {
+			log('*** nodemon crashed: script crashed for some reason');
+		})
+		.on('exit', function() {
+			log('*** nodemon exited cleanly');
+		});
+});
+
+
+function startBrowserSync() {
+	if(args.nosync || browserSync.active) {
+		return;
+	}
+
+	log('Starting browser-sync on port ' + port);
+
+	var options = {
+		proxy: 'localhost:' + port,
+		port: 7000,
+		files: [
+			config.publicFolder + '**/*.*',
+			config.publicStyles + 'styles.css',
+			config.endpoints + '**/*.*'
+		],
+		ghostMode: {
+			clicks: true,
+			location: false,
+			forms: true,
+			scroll: true
+		},
+		injectChanges: true,
+		logFileChanges: true,
+		logLevel: 'debug',
+		logPrefix: 'playlist-app',
+		notify: true,
+		reloadDelay: 1000
+	};
+
+	browserSync(options);
+}
+
+
+gulp.task('dev', function(callback) {
+	return runSequence(
+		'copy-app-dev',
+		'copy-index-dev',
+		'copy-templates-dev',
+		'serve-dev',
+		callback
+	);
+});
+
+
+
 
 function log(msg) {
 	if (typeof(msg) === 'object') {
@@ -45,4 +154,3 @@ function log(msg) {
 		$.util.log($.util.colors.blue(msg));
 	}
 }
-
