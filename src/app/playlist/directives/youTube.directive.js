4063c8d6ef02f97abd77c13d07d43f8472bf471d
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2016-03-07 21:02:52 -0500

    fixing youTube directivegit st

--- src/app/playlist/directives/youTube.directive.js
+++ src/app/playlist/directives/youTube.directive.js
@@ -5,7 +5,18 @@
 		.module('app.playlist')
 		.directive('youtube', youTubePlayer);
 
+		console.log('hey!');
+
 	youTubePlayer.$inject = ['$window'];	
+
+	function youTubePlayer($window) {
+		return {
+			templateUrl: 'app/playlist/directives/youTubePlayer.html',
+			restrict: 'E'
+			
+		};
+	}
+
 	function youTubeController() {
 		var vm = this;
 	}
